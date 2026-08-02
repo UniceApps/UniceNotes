aaf3c1cdf9363adbb2652453cd58f2e4f42d572f
hugofnm <contact@hugofnm.fr>
2026-08-02 01:29:50 +0100

    chore: update app ver and rel notes

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,7 @@
-export const APP_VERSION = '3.1.1';
+export const APP_VERSION = '3.1.2';
 export const IS_BETA = false;
 
 export const RELEASE_NOTES = {
     "info": "Vous avez mis à jour l\'application ! 🎉",
-    "subtitle": "Nouveautés :\n- Amélioration de l'expérience utilisateur\n- Correction de bugs mineurs\n\nMerci d'utiliser UniceNotes ! test cedric"
+    "subtitle": "Nouveautés :\n- Mise à jour de la connexion à ADE\n\nMerci d'utiliser UniceNotes !"
 }
